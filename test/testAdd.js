4272dcea369c391ca543577c2e94b95237d184d6
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-13 17:51:06 +0700

    init

---
+++ test/testAdd.js
@@ -0,0 +1,16 @@
+const assert = require('assert');
+const add = require('../src/a');
+
+describe('Test add function', () => {
+    it('Can add native number', () => {
+        const total = add(4, 5);
+        // if (total !== 9) throw new Error('Wrong');
+        // assert(total === 10);
+        assert.equal(total, 10);
+    });
+
+    it('Can add float point number', () => {
+        const total = add(4.1, 5.2);
+        if (total !== 9.3) throw new Error('Wrong');
+    });
+});
