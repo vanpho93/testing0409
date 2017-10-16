93c491986c3b81d6719a3232c3a8977a9005c6fb
Van Pho Nguyen <vanpho02@gmail.com>
2017-10-16 17:43:25 +0700

    init 2

--- test/models/createProduct.js
+++ test/models/createProduct.js
@@ -1,12 +1,12 @@
 const assert = require('assert');
-const Product = require('../src/db');
+const Product = require('../../src/db');
 
 describe('Test add new product', () => {
     beforeEach('Remove collection', async () => {
         await Product.remove({});
     })
 
-    it.only('Can add new product', async () => {
+    it('Can add new product', async () => {
         const pr = new Product({ name: 'ABCD', desc: 'EFGHnnn' });
         await pr.save();
         const pr2 = await Product.findOne({ name: 'ABCD' });
